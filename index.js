e8a868ef63d9a07e6a6d3dc74dc776fa5781cb08
BYAsurim <Pers0naL276276@gmail.com>
2023-04-08 00:20:26 +0300

    My-first-day-IT-incubator

---
+++ index.js
@@ -0,0 +1,59 @@
+// this is code 
+function SUM(a,b){
+
+return a+b;
+
+}
+console.log(SUM(5,6));
+
+var myName ;
+var a; 
+a = 7;
+
+var a;
+a = 7;
+var b;
+b = a;
+
+var  a = 9;
+var myFirstName = "Aleksandr";
+var myLastName = "Nikolayevich";
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+console.log(6);
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let  catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+
+console.log(FCC, fact); // Change this line
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar ++;
+
+// let myVar = 11;
+
+// // Only change code below this line
+// myVar = --myVar ;
