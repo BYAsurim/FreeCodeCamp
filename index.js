b86998d45366c7bd90f039c3f0e1912723819208
BYAsurim <Pers0naL276276@gmail.com>
2023-04-10 22:05:59 +0300

    my-third-day-in-it-incubator

--- index.js
+++ index.js
@@ -1,106 +1,104 @@
-// this is code 
-function SUM(a,b){
+// this is code
+// function SUM(a, b) {
+//   return a + b;
+// }
+// console.log(SUM(5, 6));
 
-return a+b;
+// var myName;
+// var a;
+// a = 7;
 
-}
-console.log(SUM(5,6));
+// var a;
+// a = 7;
+// var b;
+// b = a;
 
-var myName ;
-var a; 
-a = 7;
-
-var a;
-a = 7;
-var b;
-b = a;
-
-var  a = 9;
-var myFirstName = "Aleksandr";
-var myLastName = "Nikolayevich";
+// var a = 9;
+// var myFirstName = "Aleksandr";
+// var myLastName = "Nikolayevich";
 
 // Only change code below this line
-var a = 5;
-var b = 10;
-var c = "I am a";
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
 // Only change code above this line
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
-console.log(6);
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+// console.log(6);
 
 // Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
 
 // Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
 
-let catName = "Oliver";
-let  catSound = "Meow!";
+// let catName = "Oliver";
+// let catSound = "Meow!";
 
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
 
-console.log(FCC, fact); // Change this line
-let myVar = 87;
+// console.log(FCC, fact); // Change this line
+// let myVar = 87;
 
 // Only change code below this line
-myVar = myVar ++;
+// myVar = myVar++;
 
 // let myVar = 11;
 
-// // Only change code below this line
+// Only change code below this line
 // myVar = --myVar ;
 
 // let myVar = 11;
 
 // Only change code below this line
 // myVar = --myVar;
-const ourDecimal = 5.7;
-let myDecimal = 5.7;
+// const ourDecimal = 5.7;
+// let myDecimal = 5.7;
 // Only change code below this line
-const product = 2.0 * 2.5;
+// const product = 2.0 * 2.5;
 
-const quotient = 4.4 / 2.0; // Change this line
+// const quotient = 4.4 / 2.0; // Change this line
 
-const remainder = 11 % 3;
+// const remainder = 11 % 3;
 
-let a = 3;
-let b = 17;
-let c = 12;
-
-// Only change code below this line
-a += 12;
-b += 9 ;
-c += 7;
-
-let a = 11;
-let b = 9;
-let c = 3;
-
-let a = 5;
-let b = 12;
-let c = 4.6;
+// let a = 3;
+// let b = 17;
+// let c = 12;
 
 // Only change code below this line
-a *= 5;
-b *= 3 ;
-c *= 10;
+// a += 12;
+// b += 9;
+// c += 7;
 
-let a = 48;
-let b = 108;
-let c = 33;
+// let a = 11;
+// let b = 9;
+// let c = 3;
+
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
 
 // Only change code below this line
-a /=  12;
-b /=  4;
-c /=  11;
+// a *= 5;
+// b *= 3;
+// c *= 10;
+
+// let a = 48;
+// let b = 108;
+// let c = 33;
+
+// Only change code below this line
+// a /= 12;
+// b /= 4;
+// c /= 11;
 
 // const myStr = "I am a \"double quoted\" string inside \"double quotes\"." // Change this line
 
@@ -108,10 +106,10 @@ c /=  11;
 
 // const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 
-//const myStr = "This is the start. "+"This is the end."; // Change this line
+// const myStr = "This is the start. "+"This is the end."; // Change this line
 
-let myStr = "This is the first sentence. ";
-myStr += "This is the second sentence.";
+// let myStr = "This is the first sentence. ";
+// myStr += "This is the second sentence.";
 
 // Only change code below this line
 // const myName = "Alex";
@@ -123,25 +121,25 @@ myStr += "This is the second sentence.";
 // myStr += someAdjective;
 
 // Setup
-let lastNameLength = 0;
-//const lastName = "Lovelace";
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
 
 // Only change code below this line
-lastNameLength = lastName.length;
+// lastNameLength = lastName.length;
 
 // Setup
-let firstLetterOfLastName = "";
-const lastName = "Lovelace";
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
 
 // Only change code below this line
-firstLetterOfLastName = lastName[0]; // Change this line
+// firstLetterOfLastName = lastName[0]; // Change this line
 
 // Setup
 // let myStr = "Jello World";
 
-// // Only change code below this line
+// Only change code below this line
 // myStr = "Hello World"; // Change this line
-// // Only change code above this line
+// Only change code above this line
 
 // Setup
 // const lastName = "Lovelace";
@@ -152,26 +150,26 @@ firstLetterOfLastName = lastName[0]; // Change this line
 // Setup
 // const lastName = "Lovelace";
 
-// // Only change code below this line
+// Only change code below this line
 // const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
 
 // Setup
 // const lastName = "Lovelace";
 
-// // Only change code below this line
+// Only change code below this line
 // const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
 
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
 
 // Only change code below this line
-const wordBlanks = myAdjective+ " " + myNoun+" " + myVerb+ " " + myAdverb; // Change this line
+// const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; // Change this line
 // Only change code above this line
 
 // Only change code below this line
-const myArray = ["Alex", 1];
+// const myArray = ["Alex", 1];
 
 // Only change code below this line
 // const myArray = [[myArray]];
@@ -181,7 +179,7 @@ const myArray = ["Alex", 1];
 // Setup
 // const myArray = [18, 64, 99];
 // myArray[0] = 45;
-// // Only change code below this line
+// Only change code below this line
 
 // const myArray = [
 //     [1, 2, 3],
@@ -189,119 +187,484 @@ const myArray = ["Alex", 1];
 //     [7, 8, 9],
 //     [[10, 11, 12], 13, 14],
 //   ];
-  
+
 //   const myData = myArray[2][1];
 
 // Setup
 // const myArray = [["John", 23], ["cat", 2]];
 
-// // Only change code below this line
+// Only change code below this line
 // myArray.push(["dog", 3]);
 
 // Setup
 // const myArray = [["John", 23], ["cat", 2]];
 
-// // Only change code below this line
+// Only change code below this line
 // const removedFromMyArray = myArray.pop();
 
 // Setup
 // const myArray = [["John", 23], ["dog", 3]];
 // const removedFromMyArray = myArray.shift();
-// // Only change code below this line
+// Only change code below this line
 
 // Setup
 // const myArray = [["John", 23], ["dog", 3]];
 // myArray.shift();
 // myArray.unshift(["Paul", 35]);
 
-// // Only change code below this line
-const myList = [];
-myList.push(["alex",29], ["alex",29], ["alex",29], ["alex",29], ["alex",29]);
+// Only change code below this line
+// const myList = [];
+// myList.push(
+//   ["alex", 29],
+//   ["alex", 29],
+//   ["alex", 29],
+//   ["alex", 29],
+//   ["alex", 29]
+// );
 
-function reusableFunction(){
-    console.log("Hi World");
-  }
-  reusableFunction();
+// function reusableFunction() {
+//   console.log("Hi World");
+// }
+// reusableFunction();
 
-  function functionWithArgs(a, b) {
-    console.log(a+b);
-  }
-  functionWithArgs(1, 2);
+// function functionWithArgs(a, b) {
+//   console.log(a + b);
+// }
+// functionWithArgs(1, 2);
 
-  function timesFive(num) {
-    return num *5;
-  }
+// function timesFive(num) {
+//   return num * 5;
+// }
 
-  // Declare the myGlobal variable below this line
-let myGlobal  = 10;
+// Declare the myGlobal variable below this line
+// let myGlobal = 10;
 
-function fun1() {
-  oopsGlobal = 5;
-
-}
+// function fun1() {
+//   oopsGlobal = 5;
+// }
 
 // Only change code above this line
 
-function fun2() {
-  let output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+// function fun2() {
+//   let output = "";
+//   if (typeof myGlobal != "undefined") {
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined") {
+//     output += " oopsGlobal: " + oopsGlobal;
+//   }
+//   console.log(output);
+// }
 
-function myLocalScope() {
-    // Only change code below this line
-  let myVar ;
-    console.log('inside myLocalScope', myVar);
-  }
-  myLocalScope();
-  
-  // Run and check the console
-  // myVar is not defined outside of myLocalScope
-  console.log('outside myLocalScope', myVar);
+// function myLocalScope() {
+//   Only change code below this line
+//   let myVar;
+//   console.log("inside myLocalScope", myVar);
+// }
+// myLocalScope();
 
-  // Setup
-const outerWear = "T-Shirt";
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+// console.log("outside myLocalScope", myVar);
 
-function myOutfit() {
+// Setup
+// const outerWear = "T-Shirt";
+
+// function myOutfit() {
+//   Only change code below this line
+//   let outerWear = "sweater";
+//   Only change code above this line
+//   return outerWear;
+// }
+
+// myOutfit();
+
+// Setup
+// let sum = 0;
+
+// function addThree() {
+//   sum = sum + 3;
+// }
+
+// Only change code below this line
+// function addFive() {
+//   sum = sum + 5;
+// }
+
+// Only change code above this line
+
+// addThree();
+// addFive();
+
+// Setup
+// let processed = 0;
+
+// function processArg(num) {
+//   return (num + 3) / 5;
+// }
+// processed = processArg(7);
+// Only change code below this line
+
+// function nextInLine(arr, item) {
+//   arr.push(item);
+//   item = arr.shift();
+
+//   return item;
+//   Only change code above this line
+// }
+
+// Setup
+// let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+// console.log("Before: " + JSON.stringify(testArr));
+// console.log(nextInLine(testArr, 6));
+// console.log("After: " + JSON.stringify(testArr));
+
+// function welcomeToBooleans() {
+//   Only change code below this line
+
+//   return true; // Change this line
+
+//   Only change code above this line
+// }
+
+// function trueOrFalse(wasThatTrue) {
+//   Only change code below this line
+//   if (wasThatTrue) {
+//     return "Yes, that was true";
+//   }
+//   return "No, that was false";
+
+//   Only change code above this line
+// }
+// Setup
+// function testEqual(val) {
+//   if (val == 12) {
+//     Change this line
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+
+// testEqual(10);
+
+// Setup
+// function testStrict(val) {
+//   if (val === 7) {
+//     Change this line
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+
+// testStrict(10);
+
+// Setup
+// function compareEquality(a, b) {
+//   if (a === b) {
+//     Change this line
+//     return "Equal";
+//   }
+//   return "Not Equal";
+// }
+
+// compareEquality(10, "10");
+
+// Setup
+// function testNotEqual(val) {
+//   if (val != 99) {
+//     Change this line
+//     return "Not Equal";
+//   }
+//   return "Equal";
+// }
+
+// testNotEqual(10);
+
+// Setup
+// function testStrictNotEqual(val) {
+//   if (val !== 17) {
+//     Change this line
+//     return "Not Equal";
+//   }
+//   return "Equal";
+// }
+
+// testStrictNotEqual(10);
+
+// function testGreaterThan(val) {
+//   if (val > 100) {
+//     Change this line
+//     return "Over 100";
+//   }
+
+//   if (val > 10) {
+//     Change this line
+//     return "Over 10";
+//   }
+
+//   return "10 or Under";
+// }
+
+// testGreaterThan(10);
+// function testGreaterOrEqual(val) {
+//   if (val >= 20) {
+//     Change this line
+//     return "20 or Over";
+//   }
+
+//   if (val >= 10) {
+//     Change this line
+//     return "10 or Over";
+//   }
+
+//   return "Less than 10";
+// }
+
+// testGreaterOrEqual(10);
+
+// function testLessThan(val) {
+//   if (val < 25) {
+//     Change this line
+//     return "Under 25";
+//   }
+
+//   if (val < 55) {
+//     Change this line
+//     return "Under 55";
+//   }
+
+//   return "55 or Over";
+// }
+
+// testLessThan(10);
+// function testLessOrEqual(val) {
+//   if (val <= 12) {
+//     Change this line
+//     return "Smaller Than or Equal to 12";
+//   }
+
+//   if (val <= 24) {
+//     Change this line
+//     return "Smaller Than or Equal to 24";
+//   }
+
+//   return "More Than 24";
+// }
+
+// testLessOrEqual(10);
+// function testLogicalAnd(val) {
+//   Only change code below this line
+
+//   if (val <= 50 && val >= 25) {
+//     return "Yes";
+//   }
+
+//   Only change code above this line
+//   return "No";
+// }
+
+// testLogicalAnd(10);
+
+// function testLogicalOr(val) {
+//   Only change code below this line
+
+//   if (val < 10 || val > 20) {
+//     return "Outside";
+//   }
+
+//   return "Inside";
+
+//   Only change code above this line
+// }
+
+// testLogicalOr(15);
+
+// function testElse(val) {
+//   let result = "";
+//   Only change code below this line
+
+//   if (val > 5) {
+//     result = "Bigger than 5";
+//   } else {
+//     result = "5 or Smaller";
+//   }
+
+//   Only change code above this line
+//   return result;
+// }
+
+// testElse(4);
+// function testElseIf(val) {
+//   if (val > 10) {
+//     return "Greater than 10";
+//   } else if (val < 5) {
+//     return "Smaller than 5";
+//   } else {
+//     return "Between 5 and 10";
+//   }
+// }
+
+// testElseIf(7);
+// function orderMyLogic(val) {
+//   if (val < 5) {
+//     return "Less than 5";
+//   } else if (val < 10) {
+//     return "Less than 10";
+//   } else {
+//     return "Greater than or equal to 10";
+//   }
+// }
+
+// orderMyLogic(7);
+// function testSize(num) {
+//   Only change code below this line
+//   if (num < 5) {
+//     return "Tiny";
+//   } else if (num < 10) {
+//     return "Small";
+//   } else if (num < 15) {
+//     return "Medium";
+//   } else if (num < 20) {
+//     return "Large";
+//   } else {
+//     return "Huge";
+//   }
+
+//   Only change code above this line
+// }
+
+// testSize(7);
+
+// const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+// function golfScore(par, strokes) {
+//   debugger;
+//   if (strokes == "1") {
+//     return names[0];
+//   } else if (strokes <= par - 2) {
+//     return names[1];
+//   }else if (strokes == par - 1) {
+//     return names[2];
+//   } else if (strokes == par) {
+//     return names[3];
+//   } else if (strokes == par + 1) {
+//     return names[4];
+//   }else if (strokes == par + 2) {
+//     return names[5];
+//   } else if (strokes >= par + 3) {
+//     return names[6];
+//   }
+
+//   // Only change code above this line
+// }
+// // console.log(golfScore(4, 1));
+// // console.log(golfScore(4, 2));
+// // console.log(golfScore(5, 2));
+// // console.log(golfScore(4, 3));
+// console.log(golfScore(4, 4));
+
+function caseInSwitch(val) {
+  let answer = "";
   // Only change code below this line
- let outerWear = "sweater";
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+
+    case 4:
+      answer = "delta";
+      break;
+  }
+
   // Only change code above this line
-  return outerWear;
+  return answer;
 }
 
-myOutfit();
+caseInSwitch(1);
 
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
 
-// Setup
-let sum = 0;
+    default:
+      answer = "stuff";
+      break;
+  }
 
-function addThree() {
-  sum = sum + 3;
+  // Only change code above this line
+  return answer;
 }
 
-// Only change code below this line
-function addFive() {
-  sum = sum + 5;
+switchOfStuff(1);
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
 }
 
-// Only change code above this line
+sequentialSizes(1);
 
-addThree();
-addFive();
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
 
-// Setup
-let processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
+  // Only change code above this line
+  return answer;
 }
-processed = processArg(7);
-// Only change code below this line
 
-
-
+chainToSwitch(7);
